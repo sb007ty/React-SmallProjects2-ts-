37d33348c6f19800c55f89471104730b3e8b7170
spandan <spandan7b@gmail.com>
2025-01-04 18:47:58 +0530

    impl with fade out

--- src/components/notifications-proj/NotificationMsg.tsx
+++ src/components/notifications-proj/NotificationMsg.tsx
@@ -7,28 +7,28 @@ const NotificationMsg: React.FC = () => {
     NotificationMsgContext
   );
   const timer = useRef();
-  // useEffect(() => {
-  //   if (notificationMsg.length === 0) return;
-  //   timer.current = setInterval(() => {
-  //     if (notificationMsg.length === 0) clearInterval(timer.current);
-  //     const newNotification = notificationMsg.filter(
-  //       (item) => item.progress < 100
-  //     );
-  //     // console.log(newNotification, "newN***");
-  //     setNotificationMsg(newNotification);
-  //   }, 100);
-  //   return () => {
-  //     console.log(timer.current, "timer**");
-  //     clearInterval(timer.current);
-  //   };
-  // }, [notificationMsg, setNotificationMsg]);
+  useEffect(() => {
+    if (notificationMsg.length === 0) return;
+    timer.current = setInterval(() => {
+      if (notificationMsg.length === 0) clearInterval(timer.current);
+      const newNotification = notificationMsg.filter(
+        (item) => item.progress < 100
+      );
+      // console.log(newNotification, "newN***");
+      setNotificationMsg(newNotification);
+    }, 100);
+    return () => {
+      console.log(timer.current, "timer**");
+      clearInterval(timer.current);
+    };
+  }, [notificationMsg, setNotificationMsg]);
   console.log(notificationMsg, "not*");
   if (notificationMsg.length === 0) return null;
   return (
     <div className="not-msg-container">
       {notificationMsg.map((item, index) => {
-        if (item.progress < 100)
-          return <NotificationMsgElement {...item} key={item.id} />;
+        // if (item.progress < 100)
+        return <NotificationMsgElement {...item} key={item.id} />;
       })}
     </div>
   );
